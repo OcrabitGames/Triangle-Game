6f12a75009a448c18e6773a00255303ab1553dbe
christinaclements <christina.clements@bruins.belmont.edu>
2025-04-04 15:19:47 -0500

    Added Buttons and Menu Script
    Added buttons for the menu scene for play, quit and level selection. I also wrote some of the script that will change the scenes throughout the game and different levels.

--- Assets/Scripts/Tina/MenuChange.cs
+++ Assets/Scripts/Tina/MenuChange.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class MenuChange : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void goToGame()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void goToMenu()
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    public void goToLevelSelection()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LevelSelection");
+    }
+    public void goToLevel1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+    public void goToLevel2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level2");
+    }
+    public void goToLevel3()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level3");
+    }
+    public void goToLevel4()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level4");
     }
 }
