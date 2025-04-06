cdf03b936c8b3f210469158eeeaef133a882aa7f
kkwatts <kkwatts15@gmail.com>
2025-04-06 15:27:25 -0500

    Started combining scenes
    Started adding stuff from Marco's scene to begin combining everything into one scene

--- Assets/Kate/Scripts/CameraScript.cs
+++ Assets/Kate/Scripts/CameraScript.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
+    public GameObject target;
+    public Vector3 camOffset;
+    public float camSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         gameObject.GetComponent<Camera>().transparencySortMode = TransparencySortMode.Orthographic;
@@ -8,6 +12,10 @@ public class CameraScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        Move(target.transform.position, camOffset, camSpeed);
+    }
+
+    private void Move(Vector3 targetPos, Vector3 offset, float speed) {
+        transform.position = Vector3.Lerp(transform.position, targetPos + offset, speed);
     }
 }
